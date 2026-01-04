e65e28b356663e96df916c0b70e5aff0cf06dbc8
Ahmed55455 <ahmedali22@istanbularel.edu.tr>
2026-01-05 01:38:29 +0300

    Add Application Entry Point
    Added the Main class to launch the application.

---
+++ src/librarymanagementsystem/Main.java
@@ -0,0 +1,11 @@
+package librarymanagementsystem;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create the object
+        LibraryManagementSystem app = new LibraryManagementSystem();
+        
+        // Start the application
+        app.start();
+    }
+}
